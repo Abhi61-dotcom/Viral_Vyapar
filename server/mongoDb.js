6af096bf40f0bc1b7be0745df6b6f1d4c5391341
Abhishek Choudhary <jaatabhishek61@gmail.com>
2026-07-29 11:24:11 +0530

    Add dual SRV and direct seedlist connection handlers for MongoDB Atlas Cloud

--- server/mongoDb.js
+++ server/mongoDb.js
@@ -10,6 +10,7 @@ import Admin from './models/Admin.js';
 mongoose.set('bufferCommands', true);
 
 const DEFAULT_MONGO_URI = 'mongodb+srv://jaatabhishek61_db_user:tN36kivXbe6WAcbs@cluster0.sksgdt0.mongodb.net/viral_vyapar?retryWrites=true&w=majority&appName=Cluster0';
+const SEEDLIST_MONGO_URI = 'mongodb://jaatabhishek61_db_user:tN36kivXbe6WAcbs@cluster0-shard-00-00.sksgdt0.mongodb.net:27017,cluster0-shard-00-01.sksgdt0.mongodb.net:27017,cluster0-shard-00-02.sksgdt0.mongodb.net:27017/viral_vyapar?ssl=true&replicaSet=atlas-13d80b-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 let cached = global.mongooseCached;
 if (!cached) {
@@ -27,28 +28,34 @@ export const connectMongoDB = async () => {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 3000,
-      connectTimeoutMS: 3000,
-      maxPoolSize: 10
-    }).then((m) => {
-      Admin.findOne({ username: 'admin' }).then((existingAdmin) => {
-        if (!existingAdmin) {
-          const salt = bcrypt.genSaltSync(8);
-          const hashedPassword = bcrypt.hashSync('admin123', salt);
-          Admin.create({
-            username: 'admin',
-            email: 'choudharyabhishek1503@gmail.com',
-            passwordHash: hashedPassword
-          }).catch(() => {});
-        }
-      }).catch(() => {});
-      return m;
-    }).catch((err) => {
-      cached.promise = null;
-      console.warn('MongoDB connection error:', err.message);
-      return false;
-    });
+    const tryConnect = async (targetUri) => {
+      return mongoose.connect(targetUri, {
+        serverSelectionTimeoutMS: 4000,
+        connectTimeoutMS: 4000,
+        maxPoolSize: 10
+      });
+    };
+
+    cached.promise = tryConnect(uri)
+      .catch(() => tryConnect(SEEDLIST_MONGO_URI))
+      .then((m) => {
+        Admin.findOne({ username: 'admin' }).then((existingAdmin) => {
+          if (!existingAdmin) {
+            const salt = bcrypt.genSaltSync(8);
+            const hashedPassword = bcrypt.hashSync('admin123', salt);
+            Admin.create({
+              username: 'admin',
+              email: 'choudharyabhishek1503@gmail.com',
+              passwordHash: hashedPassword
+            }).catch(() => {});
+          }
+        }).catch(() => {});
+        return m;
+      }).catch((err) => {
+        cached.promise = null;
+        console.warn('MongoDB connection error:', err.message);
+        return false;
+      });
   }
 
   try {
